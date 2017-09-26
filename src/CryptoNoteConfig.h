1f9fd772782924f8fedb7b3fe1101f5a6c7a0c66
Spencer Miller <smiller@instructure.com>
2017-09-26 01:39:07 -0600

    Add genesis hex

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -70,7 +70,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 } // parameters
 
 const char     CRYPTONOTE_NAME[]                             = "jewelcoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210102851bb81292f2ec2eef3a88e9a229a3115817c044b585445f0c22050ebd3a32";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
